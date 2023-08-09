0e876cf6dce701a0bc7451fda5cc66a6aba1e033
Gabriel Lutz <gabriel.lutz5@gmail.com>
2023-08-08 23:18:55 -0300

    @

--- src/components/TopCard.tsx
+++ src/components/TopCard.tsx
@@ -1,8 +1,8 @@
 export default function TopCard() {
 	return (
 		<>
-			<div className="top-card h-10 mx-32  p-4 bg-red-500">
-
+			<div className="flex top-card h-10 mx-32 relative top-12 p-4 bg-gray-300 bg-opacity-20 backdrop-blur-md shadow-md rounded-lg">
+				<div className="foto bg-red-400 w-10 h-10"></div>
 			</div>
 		</>
 	)
